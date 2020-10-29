44f39f499e72578006f52a901ba0ed9aebd70930
jaeger-consulting <jonathan.jaeger@jaeger-consulting.io>
2020-10-29 16:40:01 +0100

    20201029T1639

--- documents/sample_size_allocation_ratio_prior_information.R
+++ documents/sample_size_allocation_ratio_prior_information.R
@@ -2,7 +2,7 @@
 # R code for sample size calculation in Bayesian setting in presence of prior information                                  #
 # Gaussian data distribution                                                                                               #
 # Created by: Jonathan Jaeger - Date: 2020-10-27                                                                           #
-# Last modified by: Jonathan Jaeger - Date: 2020-10-27                                                                     #
+# Last modified by: Jonathan Jaeger - Date: 2020-10-28                                                                     #
 #--------------------------------------------------------------------------------------------------------------------------#
 
 
@@ -16,7 +16,7 @@
 #------------------------------------------------ Compiling the stan model ------------------------------------------------#
 
    model_stan <- rstan::stan_model(file = paste('G:/03_Projects/001_Website/jaeger-consulting.github.io/documents/',
-                                                'gaussian.stan',
+                                                'sample_size_allocation_ratio_prior_information.stan',
                                                 sep = ''),
                                    verbose = FALSE)
 
@@ -29,11 +29,19 @@
       mu_ctl <- 5
       sigma_ctl <- 2
 
+      mean_mu_ctl_non_informative <- 0
+      mean_mu_ctl_informative <- mu_ctl
+      sd_mu_ctl_non_informative <- 1e3
+      sd_mu_ctl_informative <- 1.0
+
 
    #------------------------------------------------- Experimental group --------------------------------------------------#
 
       mu_exp <- 7
       sigma_exp <- 2
+
+      mean_mu_exp_non_informative <- 0
+      sd_mu_exp_non_informative <- 1e3
 
 
    #----------------------------------------- Target operational characteristics ------------------------------------------#
@@ -61,7 +69,11 @@
       no_cores <- detectCores(logical = TRUE)
       cl <- makeCluster(no_cores - 2)
       clusterExport(cl = cl,
-                    varlist = c('model_stan', 'superiority_threshold', 'power_target', 'iter', 'warmup'))
+                    varlist = c('model_stan',
+                                'mean_mu_ctl_non_informative', 'mean_mu_exp_non_informative',
+                                'sd_mu_ctl_non_informative', 'sd_mu_exp_non_informative',
+                                'superiority_threshold', 'power_target',
+                                'iter', 'warmup'))
       clusterEvalQ(cl = cl,
                    expr = {require(StanHeaders); require(rstan)})
 
@@ -106,8 +118,8 @@
                                            fit_ctl <- sampling(object = model_stan,
                                                                data = list(N = x[1],
                                                                            y = as.numeric(y_ctl),
-                                                                           mean_mu = 0,
-                                                                           sd_mu = 1e6),
+                                                                           mean_mu = mean_mu_ctl_non_informative,
+                                                                           sd_mu = sd_mu_ctl_non_informative),
                                                                init = function() { list(mu = mean(y_ctl),
                                                                                         tau = 1 / var(y_ctl)) },
                                                                pars = c('mu', 'sigma'),
@@ -122,8 +134,8 @@
                                            fit_exp <- sampling(object = model_stan,
                                                                data = list(N = x[4],
                                                                            y = as.numeric(y_exp),
-                                                                           mean_mu = 0,
-                                                                           sd_mu = 1e6),
+                                                                           mean_mu = mean_mu_exp_non_informative,
+                                                                           sd_mu = sd_mu_exp_non_informative),
                                                                init = function() { list(mu = mean(y_exp),
                                                                                         tau = 1 / var(y_exp)) },
                                                                pars = c('mu', 'sigma'),
@@ -157,7 +169,11 @@
       no_cores <- detectCores(logical = TRUE)
       cl <- makeCluster(no_cores - 2)
       clusterExport(cl = cl,
-                    varlist = c('model_stan', 'superiority_threshold', 'power_target', 'iter', 'warmup'))
+                    varlist = c('model_stan',
+                                'mean_mu_ctl_non_informative', 'mean_mu_exp_non_informative',
+                                'sd_mu_ctl_non_informative', 'sd_mu_exp_non_informative',
+                                'superiority_threshold', 'power_target',
+                                'iter', 'warmup'))
       clusterEvalQ(cl = cl,
                    expr = {require(StanHeaders); require(rstan)})
 
@@ -202,8 +218,8 @@
                                            fit_ctl <- sampling(object = model_stan,
                                                                data = list(N = x[1],
                                                                            y = as.numeric(y_ctl),
-                                                                           mean_mu = 0,
-                                                                           sd_mu = 1e6),
+                                                                           mean_mu = mean_mu_ctl_non_informative,
+                                                                           sd_mu = sd_mu_ctl_non_informative),
                                                                init = function() { list(mu = mean(y_ctl),
                                                                                         tau = 1 / var(y_ctl)) },
                                                                pars = c('mu', 'sigma'),
@@ -218,8 +234,8 @@
                                            fit_exp <- sampling(object = model_stan,
                                                                data = list(N = x[4],
                                                                            y = as.numeric(y_exp),
-                                                                           mean_mu = 0,
-                                                                           sd_mu = 1e6),
+                                                                           mean_mu = mean_mu_exp_non_informative,
+                                                                           sd_mu = sd_mu_exp_non_informative),
                                                                init = function() { list(mu = mean(y_exp),
                                                                                         tau = 1 / var(y_exp)) },
                                                                pars = c('mu', 'sigma'),
@@ -254,7 +270,11 @@
       no_cores <- detectCores(logical = TRUE)
       cl <- makeCluster(no_cores - 2)
       clusterExport(cl = cl,
-                    varlist = c('model_stan', 'superiority_threshold', 'power_target', 'iter', 'warmup'))
+                    varlist = c('model_stan',
+                                'mean_mu_ctl_informative', 'mean_mu_exp_non_informative',
+                                'sd_mu_ctl_informative', 'sd_mu_exp_non_informative',
+                                'superiority_threshold', 'power_target',
+                                'iter', 'warmup'))
       clusterEvalQ(cl = cl,
                    expr = {require(StanHeaders); require(rstan)})
 
@@ -290,8 +310,8 @@
                                            fit_ctl <- sampling(object = model_stan,
                                                                data = list(N = x[1],
                                                                            y = as.numeric(y_ctl),
-                                                                           mean_mu = x[2],
-                                                                           sd_mu = 1.0),
+                                                                           mean_mu = mean_mu_ctl_informative,
+                                                                           sd_mu = sd_mu_ctl_informative),
                                                                init = function() { list(mu = mean(y_ctl),
                                                                                         tau = 1 / var(y_ctl)) } ,
                                                                pars = c('mu', 'sigma'),
@@ -306,8 +326,8 @@
                                            fit_exp <- sampling(object = model_stan,
                                                                data = list(N = x[4],
                                                                            y = as.numeric(y_exp),
-                                                                           mean_mu = 0,
-                                                                           sd_mu = 1e6),
+                                                                           mean_mu = mean_mu_exp_non_informative,
+                                                                           sd_mu = sd_mu_exp_non_informative),
                                                                init = function() { list(mu = mean(y_exp),
                                                                                         tau = 1 / var(y_exp)) } ,
                                                                pars = c('mu', 'sigma'),
@@ -343,7 +363,11 @@
       no_cores <- detectCores(logical = TRUE)
       cl <- makeCluster(no_cores - 2)
       clusterExport(cl = cl,
-                    varlist = c('model_stan', 'superiority_threshold', 'power_target', 'iter', 'warmup'))
+                    varlist = c('model_stan',
+                                'mean_mu_ctl_informative', 'mean_mu_exp_non_informative',
+                                'sd_mu_ctl_informative', 'sd_mu_exp_non_informative',
+                                'superiority_threshold', 'power_target',
+                                'iter', 'warmup'))
       clusterEvalQ(cl = cl,
                    expr = {require(StanHeaders); require(rstan)})
 
@@ -396,8 +420,8 @@
                                               fit_ctl <- sampling(object = model_stan,
                                                                   data = list(N = x[1],
                                                                               y = as.numeric(y_ctl),
-                                                                              mean_mu = x[2],
-                                                                              sd_mu = 1.0),
+                                                                              mean_mu = mean_mu_ctl_informative,
+                                                                              sd_mu = sd_mu_ctl_informative),
                                                                   init = function() { list(mu = mean(y_ctl),
                                                                                            tau = 1 / var(y_ctl)) } ,
                                                                   pars = c('mu', 'sigma'),
@@ -412,8 +436,8 @@
                                               fit_exp <- sampling(object = model_stan,
                                                                   data = list(N = x[4],
                                                                               y = as.numeric(y_exp),
-                                                                              mean_mu = 0,
-                                                                              sd_mu = 1e6),
+                                                                              mean_mu = mean_mu_exp_non_informative,
+                                                                              sd_mu = sd_mu_exp_non_informative),
                                                                   init = function() { list(mu = mean(y_exp),
                                                                                            tau = 1 / var(y_exp)) } ,
                                                                   pars = c('mu', 'sigma'),
@@ -444,4 +468,4 @@
 
 #------------------------------------------ Saving image of all the simulations -------------------------------------------#
 
-   save.image('G:/03_Projects/001_Website/jaeger-consulting.github.io/documents/allocation_ratio_prior_information.RData')
+   save.image('G:/03_Projects/001_Website/jaeger-consulting.github.io/documents/sample_size_allocation_ratio_prior_information.RData')
